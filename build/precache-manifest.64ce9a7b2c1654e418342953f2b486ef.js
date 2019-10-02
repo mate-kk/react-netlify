d8898a8b59eaa3837422b85efeace475c4666bbe
Máté Kis-Király <mate.kis-kiraly@nemesys.fi>
2019-10-03 00:47:26 +0300

    Deploying?

---
+++ build/precache-manifest.64ce9a7b2c1654e418342953f2b486ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcb285693b944bc30db1a51a191d943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbda1c3ef37be51eb8d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "927ff96535b9e1c56fb9",
+    "url": "/static/js/2.ce24418d.chunk.js"
+  },
+  {
+    "revision": "cdbda1c3ef37be51eb8d",
+    "url": "/static/js/main.0d3410c5.chunk.js"
+  },
+  {
+    "revision": "9c6e02de9968e8abff44",
+    "url": "/static/js/runtime-main.5bff9cce.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
